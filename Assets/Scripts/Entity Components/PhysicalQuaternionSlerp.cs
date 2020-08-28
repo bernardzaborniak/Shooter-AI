ce7c9454ba5fb2369c79ed32db8032d99003b0cb
Master-Bernardo <benito4747@live.com>
2020-08-28 11:56:35 +0200

    fixed problem with switching angular velocity

--- Assets/Scripts/Entity Components/PhysicalQuaternionSlerp.cs
+++ Assets/Scripts/Entity Components/PhysicalQuaternionSlerp.cs
@@ -26,6 +26,7 @@ public class PhysicalQuaternionSlerp : MonoBehaviour
     float defaultTime;   // time it would take to perform the rotation if initial velocity would be 0 degrees/s
     float rotationTime;  //time it takes to perform the rotation
     float switchDirectionTime;  //time when we start applying acceleration in the different direction
+    bool isBraking;
 
     float distanceTraveledAtSwitch;
 
@@ -56,24 +57,41 @@ public class PhysicalQuaternionSlerp : MonoBehaviour
 
             float distanceTraveled;
 
-            if (deltaTime< switchDirectionTime)
+            if (!isBraking)
             {
-                distanceTraveled = initialVelocity * deltaTime + 0.5f * acceleration * deltaTime * deltaTime;
-                distanceTraveledAtSwitch = distanceTraveled;
+                if(deltaTime < switchDirectionTime) 
+                { 
+                    distanceTraveled = initialVelocity * deltaTime + 0.5f * acceleration * deltaTime * deltaTime;
+                }
+                else
+                {
+                    isBraking = true;
+
+                    //calculate distance traveled before the swithc, set the distanceTraveledAtSwitchValue
+                    distanceTraveledAtSwitch = initialVelocity * switchDirectionTime + 0.5f * acceleration * switchDirectionTime * switchDirectionTime;
+                    Debug.Log("switch--------------------------------");
+
+                    //calculate distance draveled after the switch
+                    float deltaTimeBraking = (deltaTime - switchDirectionTime); 
+                    distanceTraveled = distanceTraveledAtSwitch + (maxVelocity * deltaTimeBraking + 0.5f * -acceleration * deltaTimeBraking * deltaTimeBraking);
+                }
             }
             else
             {
-                float deltaTimeSecond = (deltaTime - switchDirectionTime);
-
-                distanceTraveled = distanceTraveledAtSwitch + (maxVelocity * deltaTimeSecond + 0.5f * -acceleration * deltaTimeSecond * deltaTimeSecond);
+                float deltaTimeBraking = (deltaTime - switchDirectionTime);
+                distanceTraveled = distanceTraveledAtSwitch + (maxVelocity * deltaTimeBraking + 0.5f * -acceleration * deltaTimeBraking * deltaTimeBraking);
             }
-            //Debug.Log("deltaTime: " + deltaTime);
 
-            Debug.Log("distanceTraveled: " + distanceTraveled);
-            
-            angularVelocity = (distanceTraveled - distanceTraveledLastFrame) / deltaTime;
+
+            //Debug.Log("---------------------------------------");
+            //Debug.Log("deltaTime: " + deltaTime);
+            //Debug.Log("switchDirectionTime: " + switchDirectionTime);
+           // Debug.Log("distanceTraveled: " + distanceTraveled);
+            //Debug.Log("distanceTraveledLastFrame: " + distanceTraveledLastFrame);
+           // Debug.Log("Time.deltaTime: " + Time.deltaTime);
+            angularVelocity = (distanceTraveled - distanceTraveledLastFrame) / Time.deltaTime;
             distanceTraveledLastFrame = distanceTraveled;
-            //Debug.Log("angular Vel: " + angularVelocity);
+            Debug.Log("angular Vel: " + angularVelocity);
 
             float percentageOfRotationTraveled = distanceTraveled/ angleDistance;  // 0 to 1;
             //Debug.Log("percentageOfRotationTraveled: " + percentageOfRotationTraveled);
@@ -85,6 +103,8 @@ public class PhysicalQuaternionSlerp : MonoBehaviour
 
     public void Rotate(Quaternion targeRotation)
     {
+        isBraking = false;
+
         Debug.Log("rotate");
         currentTargetRot = targeRotation;
         currentStartRotatingRotation = transformToRotate.rotation;
