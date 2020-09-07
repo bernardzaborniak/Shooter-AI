26bc661e9a6ecee78ae207f7ecca2ba17c6b0b79
Master-Bernardo <benito4747@live.com>
2020-09-07 17:21:01 +0200

    quicksave while developing aiming

--- Assets/Scripts/Entity Components/EC_HumanoidAimingController.cs
+++ Assets/Scripts/Entity Components/EC_HumanoidAimingController.cs
@@ -45,7 +45,16 @@ public class EC_HumanoidAimingController : EntityComponent
     public Transform lookAtTarget;
     public FLookAnimator fLookAnimator;
 
-  
+    [Header("Aiming the Weapon")]
+    public Transform weaponAimTarget;
+    public MultiAimConstraint weaponAimingConstraint;
+   
+   // public Transform aimingHand;
+   // public float movementSpeed;
+   // public float maxAngleDifference; //angle difference between desired rotation and current
+    bool aimWeapon;
+
+
 
     enum AimAtTargetingMethod
     {
@@ -92,7 +101,15 @@ public class EC_HumanoidAimingController : EntityComponent
         {
             StopAimAt();
         }
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AimWeaponAtTarget();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StopAimingWeaponAtTarget();
+        }
+
 
         if (aimAtActive)
         {
@@ -100,11 +117,11 @@ public class EC_HumanoidAimingController : EntityComponent
             // Basically all aim At Modes work the same that we have a point we are looking at in space, just the calculation of this point differs
             pointToAimAt = Vector3.zero;
 
-            if(currentAimAtTargetingMethod == AimAtTargetingMethod.Direction)
+            if (currentAimAtTargetingMethod == AimAtTargetingMethod.Direction)
             {
                 pointToAimAt = transform.position + aimDirection * defaultDirectionAimDistance + Vector3.up * defaultDirectionAimHeightOffset;
             }
-            else if(currentAimAtTargetingMethod == AimAtTargetingMethod.Position)
+            else if (currentAimAtTargetingMethod == AimAtTargetingMethod.Position)
             {
                 pointToAimAt = aimAtPosition;
             }
@@ -122,7 +139,7 @@ public class EC_HumanoidAimingController : EntityComponent
 
             // 3.1 Limit to rotation on the x axis only
             //We are kind of rotating the vector to point in the Forward Direction
-            desiredSpineDirection = new Vector3(0, directionToAim.y, new Vector2(directionToAim.x, directionToAim.z).magnitude);  
+            desiredSpineDirection = new Vector3(0, directionToAim.y, new Vector2(directionToAim.x, directionToAim.z).magnitude);
             desiredSpineDirection = transform.TransformDirection(desiredSpineDirection);
 
             // 3.2 Set the target weight of spine constraints depending on if the target is above or below the shoulders
@@ -162,6 +179,25 @@ public class EC_HumanoidAimingController : EntityComponent
         spineConstraint2.weight = spineConstraint2CurrentWeight;
         spineConstraint3CurrentWeight += Mathf.Clamp((spineConstraint3TargetWeight - spineConstraint3.weight), -maxSpeed, maxSpeed);
         spineConstraint3.weight = spineConstraint3CurrentWeight;
+
+        if (aimWeapon)
+        {
+            weaponAimTarget.position = aimAtTarget.position;
+        }
+    }
+
+    public override void LateUpdateComponent()
+    {
+        if (aimWeapon)
+        {
+            //Vector3 previousArmUp = aimingHand.forward;
+            //aimingHand.forward = Vector3.up;
+            //aimingHand.up = (aimAtTarget.position - aimingHand.position);
+            //aimingHand.forward = previousArmUp;
+            ////aimingHand.rotation = Quaternion.LookRotation((aimAtTarget.position - aimingHand.position), -aimingHand.up); //multiply with euler, to make lookRotation Aim At Y and not z axies
+            //aimingHand.localRotation *= Quaternion.Euler(-90f, 180f, 0f);
+            // aimingHand.LookAt(aimAtTarget, aimingHand.forward); 
+        }
     }
 
 
@@ -212,10 +248,23 @@ public class EC_HumanoidAimingController : EntityComponent
         movementController.manualRotation = false;
     }
 
-    private void OnDrawGizmos()
+    public void AimWeaponAtTarget()//Transform target)
+    {
+        weaponAimingConstraint.weight = 1;
+        aimWeapon = true;
+    }
+
+    public void StopAimingWeaponAtTarget()
+    {
+        weaponAimingConstraint.weight = 0;
+        aimWeapon = false;
+
+    }
+
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
 
         Gizmos.DrawLine(aimingDistanceReferencePoint.position, aimingDistanceReferencePoint.position + desiredSpineDirection);
-    }
+    }*/
 }
