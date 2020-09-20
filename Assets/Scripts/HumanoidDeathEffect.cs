032dfae8b86833ced41b41998f58d1d4c31f70bd
Master-Bernardo <benito4747@live.com>
2020-09-20 18:07:10 +0200

    death effect is now copying the character animation

--- Assets/Scripts/HumanoidDeathEffect.cs
+++ Assets/Scripts/HumanoidDeathEffect.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class HumanoidDeathEffect : MonoBehaviour
 {
+    [Serializable]
+    public class BoneTransformCopyHelper
+    {
+        public Transform originalBone;
+        public Transform corpseBone;
+    }
 
+    public BoneTransformCopyHelper[] boneTransformCopyHelpers;
+
+
+    //public Rigidbody
 
     void Start()
     {
@@ -16,10 +27,24 @@ public class HumanoidDeathEffect : MonoBehaviour
         
     }
 
-    public void EnableDeathEffect()
+    public void EnableDeathEffect()//Vector3 movementForceAtTimeOfDeath, Vector3 impactForceOfKillingDamage, Vector3 killingDamagePosition)
     {
         Debug.Log("Enable death effect");
         transform.SetParent(null);
+
+        CopyOriginalBonePositionsToCorpse();
+
         gameObject.SetActive(true);
+
+    }
+
+    void CopyOriginalBonePositionsToCorpse()
+    {
+        // The corpse may be missing come bones if the head or somehting was blown off
+        foreach(BoneTransformCopyHelper helper in boneTransformCopyHelpers)
+        {
+            helper.corpseBone.position = helper.originalBone.position;
+            helper.corpseBone.rotation = helper.originalBone.rotation;
+        }
     }
 }
