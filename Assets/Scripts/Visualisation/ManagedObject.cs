11f368016b6f968ddaf94e7d114ee9f6dcb2777e
Master-Bernardo <benito4747@live.com>
2020-11-21 17:45:54 +0100

    added text to camera alignement

---
+++ Assets/Scripts/Visualisation/ManagedObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagedObject : MonoBehaviour
+{
+
+    //You can add the script to the singleton manager either on start or on enable, 
+    //if you use onEnable, make sure the manager is executed before the managedObjects 
+    //in the scriptExecution Order serrings to prevent null Exceptions
+   
+    /*private void OnEnable()
+    {
+        SingletonManager.Instance.AddManagedObject(this);
+    }*/
+
+    void Start()
+    {
+        SingletonManager.Instance.AddManagedObject(this);
+    }
+
+    void OnDisable()
+    {
+        SingletonManager.Instance.RemoveManagedObject(this);
+    }
+
+    public void UpdateManagedObject(Vector3 cameraForward)
+    {
+        transform.forward = cameraForward;
+    }
+}
---
+++ Assets/Scripts/Visualisation/SingletonManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonManager : MonoBehaviour
+{
+    public Transform camTransform;
+    public HashSet<ManagedObject> managedObjects = new HashSet<ManagedObject>();
+
+
+    #region Singleton Code
+    public static SingletonManager Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(Instance);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+
+    void Update()
+    {
+        Vector3 camForward = camTransform.forward;
+
+        foreach (ManagedObject obj in managedObjects)
+        {
+            obj.UpdateManagedObject(camForward);
+        }
+    }
+
+    public void AddManagedObject(ManagedObject obj)
+    {
+        managedObjects.Add(obj);
+    }
+
+    public void RemoveManagedObject(ManagedObject obj)
+    {
+        managedObjects.Remove(obj);
+    }
+}
