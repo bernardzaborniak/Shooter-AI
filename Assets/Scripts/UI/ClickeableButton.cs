25012962d6cc80c1365da984455771510f605053
Master-Bernardo <benito4747@live.com>
2020-11-26 16:51:45 +0100

    prepared UI for visualisation manager

---
+++ Assets/Scripts/UI/ClickeableButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+//simple clickeable button, cause ive had problems with the unity one
+public class ClickeableButton : MonoBehaviour, IPointerClickHandler
+{
+    public UnityEvent OnLmB;
+    public UnityEvent OnRmb;
+    public Image image;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLmB.Invoke();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRmb.Invoke();
+        }
+    }
+
+    public void SetColor(Color color)
+    {
+        //setting image thwice because otherwise this custom shader wont notice the color change
+        image.color = color;
+        image.color = color;
+    }
+}
---
+++ Assets/Scripts/UI/ToogleableButton.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+public class ToogleableButton : MonoBehaviour, IPointerClickHandler
+{
+    public bool active;
+
+    [Header("Event Logic")]
+    public UnityEvent OnActivate;
+    public UnityEvent OnDeactivate;
+
+
+    public enum ToogleableButtonType
+    {
+        ChangeColor,
+        ChangeImage
+    }
+
+    [Header("Visuals")]
+    public ToogleableButtonType buttonType;
+
+    //-----type == ChangeColor---------
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeColor)]
+    public Color activeColor;
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeColor)]
+    public Color inactiveColor;
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeColor)]
+    public Image imageWhichColorToChange;
+
+
+    //-----type == ChangeImage---------
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeImage)]
+    public Sprite activeSprite;
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeImage)]
+    public Sprite inactiveSprite;
+    [ShowWhen("buttonType", ToogleableButtonType.ChangeImage)]
+    public Image imageWhichSpriteToChange;
+
+
+    private void Start()
+    {
+        if (active)
+        {
+            ActivateVisuals();
+        }
+        else
+        {
+            DeactivateVisuals();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        active = !active;
+
+        if (active)
+        {
+            OnActivate.Invoke();
+
+            ActivateVisuals();
+        }
+        else
+        {
+            OnDeactivate.Invoke();
+
+            DeactivateVisuals();
+        }
+    }
+
+    void ActivateVisuals()
+    {
+        if (buttonType == ToogleableButtonType.ChangeColor)
+        {
+            imageWhichColorToChange.color = activeColor;
+        }
+        else if (buttonType == ToogleableButtonType.ChangeImage)
+        {
+            imageWhichSpriteToChange.sprite = activeSprite;
+        }
+    }
+
+    void DeactivateVisuals()
+    {
+        if (buttonType == ToogleableButtonType.ChangeColor)
+        {
+            imageWhichColorToChange.color = inactiveColor;
+        }
+        else if (buttonType == ToogleableButtonType.ChangeImage)
+        {
+            imageWhichSpriteToChange.sprite = inactiveSprite;
+        }
+    }
+
+    public void ActivateByScriptExternally()
+    {
+
+    }
+
+    public void DeactivateByScriptExternally()
+    {
+
+    }
+
+
+}
