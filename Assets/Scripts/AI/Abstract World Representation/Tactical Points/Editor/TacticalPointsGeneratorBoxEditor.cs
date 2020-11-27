179a91233106bf5919f90fb40e8ef97d6e6b157e
Master-Bernardo <benito4747@live.com>
2020-11-27 18:00:00 +0100

    prepared custom editor with button for the generator

---
+++ Assets/Scripts/AI/Abstract World Representation/Tactical Points/Editor/TacticalPointsGeneratorBoxEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TacticalPointsGeneratorBox))]
+public class TacticalPointsGeneratorBoxEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); //this methiod draws the deault editor, we can add more custom editors later
+
+        TacticalPointsGeneratorBox myTacticalPointsGeneratorBox = (TacticalPointsGeneratorBox)target;
+        //myTacticalPointsGeneratorBox.testInt = EditorGUILayout.IntField("testIntef", myTacticalPointsGeneratorBox.testInt);
+        EditorGUILayout.LabelField("TestLabel", "second label value");
+        if(GUILayout.Button("Generate Points"))
+        {
+            myTacticalPointsGeneratorBox.Generate();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Abstract World Representation/Tactical Points/TacticalPointsGeneratorBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticalPointsGeneratorBox : MonoBehaviour
+{
+    HashSet<TacticalPoint> tacticalPointsGenerated;
+    [Range(0,5)]
+    public int testInt;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Generate()
+    {
+        Debug.Log("Genrate clicked");
+    }
+}
