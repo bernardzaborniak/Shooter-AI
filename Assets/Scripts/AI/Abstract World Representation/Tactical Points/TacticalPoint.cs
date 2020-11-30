d4c835d3f16d7c45d1e6f917e575208f30358599
Master-Bernardo <benito4747@live.com>
2020-11-30 13:02:55 +0100

    raycasts are no generated on a grid instead of random

--- Assets/Scripts/AI/Abstract World Representation/Tactical Points/TacticalPoint.cs
+++ Assets/Scripts/AI/Abstract World Representation/Tactical Points/TacticalPoint.cs
@@ -7,7 +7,7 @@ public enum TacticalPointType
     OpenFieldPoint,
     CoverPoint,
     CoverShootPoint
-    
+
 }
 
 [ExecuteInEditMode]
@@ -17,7 +17,7 @@ public class TacticalPoint : MonoBehaviour
     public GameEntity usingEntity;
     [Space(5)]
     public PointCoverRating coverRating;
-   
+
     [Tooltip("only used if  type is CoverShootPoint")]
     [ShowWhen("tacticalPointType", TacticalPointType.CoverPoint)]
     public TacticalPoint[] coverShootPoints; //or ShotPositions
@@ -73,7 +73,7 @@ public class TacticalPoint : MonoBehaviour
     #region Update Cover Shoot Points inside Editor
 
 #if UNITY_EDITOR
- 
+
     void Update()
     {
         if (!Application.isPlaying)
@@ -95,7 +95,7 @@ public class TacticalPoint : MonoBehaviour
                 }
             }
         }
-       
+
     }
 #endif
 
@@ -133,7 +133,7 @@ public class TacticalPoint : MonoBehaviour
 
     public void OnEntityExitsPoint(GameEntity entity)
     {
-        if(usingEntity == entity)
+        if (usingEntity == entity)
         {
             usingEntity = null;
         }
@@ -149,114 +149,106 @@ public class TacticalPoint : MonoBehaviour
         TacticalPointsManager.Instance.RemoveTacticalPoint(this);
     }
 
-    public void BakeCoverRatings(float crouchedHeight, float standingHeight, float numberOfRaycasts, LayerMask raycastLayerMask)
+    public void BakeCoverRatings(float crouchedHeight, float standingHeight, float raycastFactor, LayerMask raycastLayerMask)
     {
         Debug.Log("Bake Distance");
-
-        //1. convert directions into local Space
-        Vector3[] raycastDirectionsInWorldSpace = new Vector3[8];
-
-        for (int i = 0; i < 8; i++)
-        {
-            raycastDirectionsInWorldSpace[i] = transform.TransformDirection(raycastDirectionsInLocalSpace[i]);
-        }
-
 
         //2. cast and save Raycasts
         raycastsUsedForGeneratingRating.Clear();
 
-        float randomiseRange = 22.5f;
+        //float randomiseRange = 22.5f;
 
         for (int i = 0; i < 8; i++)
         {
-            if (i == 3 || i == 4)
+
+            raycastsUsedForCurrentDirection.Clear();
+
+            Vector3 rayStartPoint = transform.position + new Vector3(0, standingHeight, 0) + Random.insideUnitSphere * radius;
+
+            //Dont randomise the direction -  use a grid instead
+            float directionGridSize = 45 / raycastFactor;
+            for (float x = -22.5f; x < 22.5; x = x + directionGridSize)
             {
-
-
-                for (int j = 0; j < numberOfRaycasts; j++)
+                for (float y = -22.5f; y < 22.5; y = y + directionGridSize)
                 {
-                    //standing rating
+                    // take a forward transform, rotate it by the grids y and x offset, later roatte it around y axies - the quaternion order of application is inverse (right to left)
+                    Vector3 rotatedRaycastDirectionInLocalSpace = Quaternion.AngleAxis(i * 45, transform.up) * Quaternion.Euler(x + directionGridSize / 2, y + directionGridSize / 2, 0f) * Vector3.forward;
 
-                    //choose random point inside sphere
-                    Vector3 rayStartPoint = transform.position + new Vector3(0, standingHeight, 0) + Random.insideUnitSphere * radius;
-
-                    //randomise Direction
-                    Vector3 randomsedDirection = Quaternion.Euler(Random.Range(-randomiseRange, randomiseRange), Random.Range(-randomiseRange, randomiseRange), 0f) * raycastDirectionsInWorldSpace[i];
+                    //this could be left out if the point wouldn not be aligned to the world forward
+                    Vector3 raycastDirectionInWorldSpace = transform.TransformDirection(rotatedRaycastDirectionInLocalSpace);
 
                     RaycastHit hit;
-                    if (Physics.Raycast(rayStartPoint, randomsedDirection, out hit, Mathf.Infinity, raycastLayerMask, QueryTriggerInteraction.Ignore))
+                    if (Physics.Raycast(rayStartPoint, raycastDirectionInWorldSpace, out hit, Mathf.Infinity, raycastLayerMask, QueryTriggerInteraction.Ignore))
                     {
                         raycastsUsedForGeneratingRating.Add(new UsedRaycast(rayStartPoint, hit.point));
                         raycastsUsedForCurrentDirection.Add(new UsedRaycast(rayStartPoint, hit.point));
-                        //Debug.Log("hit: " + i + " distance = " + new UsedRaycast(rayStartPoint, hit.point).distance);
-                        Debug.Log("Raycast Hit: " + hit.collider.gameObject + " on position: " + hit.point);
                     }
                     else
                     {
-                        raycastsUsedForGeneratingRating.Add(new UsedRaycast(rayStartPoint, rayStartPoint + randomsedDirection * 100, true));
-                        //raycastsUsedForCurrentDirection.Add(new UsedRaycast(rayStartPoint, rayStartPoint + randomsedDirection * maxRaycastDistance));
+                        raycastsUsedForGeneratingRating.Add(new UsedRaycast(rayStartPoint, rayStartPoint + raycastDirectionInWorldSpace * 100, true));
 
                     }
                 }
-
-                float allDistancesCombined = 0;
-                //go through all the ratings and add the mean value to the cuirrent rating
-                foreach (UsedRaycast raycast in raycastsUsedForCurrentDirection)
-                {
-                    allDistancesCombined += raycast.distance;
-                }
-
-                float meanDistance;
-                if (raycastsUsedForCurrentDirection.Count > 0)
-                {
-                    meanDistance = allDistancesCombined / raycastsUsedForCurrentDirection.Count;
-                }
-                else
-                {
-                    meanDistance = Mathf.Infinity;
-                }
-                Debug.Log("i: " + i + " raycastsUsedForCurrentDirection.Count: " + raycastsUsedForCurrentDirection.Count);
-                Debug.Log("i: " + i + " mean distance: " + meanDistance);
-
-                coverRating.standingDistanceRating[i] = meanDistance;
-
-
-                //Calculate cover Quality: 
-                //Average absolute deviation contributes towards the quality
-                float allDeviationsCombined = 0;
-                foreach (UsedRaycast raycast in raycastsUsedForCurrentDirection)
-                {
-                    allDeviationsCombined += Mathf.Abs(raycast.distance - meanDistance);
-                    Debug.Log("i: " + i + " deviation added : " + Mathf.Abs(raycast.distance - meanDistance));
-                    Debug.Log("i: " + i + " deviation after adding: : " + allDeviationsCombined);
-
-                }
-                Debug.Log("i: " + i + " raycastsUsedForCurrentDirection.Count: " + raycastsUsedForCurrentDirection.Count);
-                Debug.Log("i: " + i + " allDeviationsCombined: " + allDeviationsCombined);
-
-                float averageAbsoluteDeviation;
-                if (raycastsUsedForCurrentDirection.Count > 0)
-                {
-                    averageAbsoluteDeviation = allDeviationsCombined / raycastsUsedForCurrentDirection.Count;
-                }
-                else
-                {
-                    averageAbsoluteDeviation = Mathf.Infinity;
-                }
-                Debug.Log("i: " + i + " averageAbsoluteDeviation: " + averageAbsoluteDeviation);
-
-                coverRating.standingQualityRating[i] = averageAbsoluteDeviation;
-                //try it once with mittlere Abweichung and once with Median der Abweichungen
-
-
-                raycastsUsedForCurrentDirection.Clear();
             }
+
+
+
+            float allDistancesCombined = 0;
+            //go through all the ratings and add the mean value to the cuirrent rating
+            foreach (UsedRaycast raycast in raycastsUsedForCurrentDirection)
+            {
+                allDistancesCombined += raycast.distance;
+            }
+
+            float meanDistance;
+            if (raycastsUsedForCurrentDirection.Count > 0)
+            {
+                meanDistance = allDistancesCombined / raycastsUsedForCurrentDirection.Count;
+            }
+            else
+            {
+                meanDistance = Mathf.Infinity;
+            }
+            // Debug.Log("i: " + i + " raycastsUsedForCurrentDirection.Count: " + raycastsUsedForCurrentDirection.Count);
+            // Debug.Log("i: " + i + " mean distance: " + meanDistance);
+
+            coverRating.standingDistanceRating[i] = meanDistance;
+
+
+            //Calculate cover Quality: 
+            //Average absolute deviation contributes towards the quality
+            float allDeviationsCombined = 0;
+            foreach (UsedRaycast raycast in raycastsUsedForCurrentDirection)
+            {
+                allDeviationsCombined += Mathf.Abs(raycast.distance - meanDistance);
+                // Debug.Log("i: " + i + " deviation added : " + Mathf.Abs(raycast.distance - meanDistance));
+                //Debug.Log("i: " + i + " deviation after adding: : " + allDeviationsCombined);
+
+            }
+            //Debug.Log("i: " + i + " raycastsUsedForCurrentDirection.Count: " + raycastsUsedForCurrentDirection.Count);
+            //Debug.Log("i: " + i + " allDeviationsCombined: " + allDeviationsCombined);
+
+            float averageAbsoluteDeviation;
+            if (raycastsUsedForCurrentDirection.Count > 0)
+            {
+                averageAbsoluteDeviation = allDeviationsCombined / raycastsUsedForCurrentDirection.Count;
+            }
+            else
+            {
+                averageAbsoluteDeviation = Mathf.Infinity;
+            }
+            // Debug.Log("i: " + i + " averageAbsoluteDeviation: " + averageAbsoluteDeviation);
+
+            coverRating.standingQualityRating[i] = averageAbsoluteDeviation;
+            //try it once with mittlere Abweichung and once with Median der Abweichungen
 
         }
     }
 
     private void OnDrawGizmos()
     {
+        //Debug.Log("Gizmos: -------  raycastsUsedForGeneratingRating.Count: " + raycastsUsedForGeneratingRating.Count);
+
         foreach (UsedRaycast item in raycastsUsedForGeneratingRating)
         {
             Gizmos.DrawLine(item.start, item.end);
