f650dd26ea9c8dd9693a3d0f0efa6297b23600de
Master-Bernardo <benito4747@live.com>
2020-12-03 13:11:46 +0100

    save while improving open field points generation

--- Assets/Scripts/AI/Abstract World Representation/Tactical Points/TacticalPointsGeneratorBox.cs
+++ Assets/Scripts/AI/Abstract World Representation/Tactical Points/TacticalPointsGeneratorBox.cs
@@ -20,6 +20,9 @@ public class TacticalPointsGeneratorBox : MonoBehaviour
     public int generatedPointCapacity;
     BoxCollider generatorBoundingBox;
 
+    public float minDistanceOfGeneratedPointToNavmeshVertex;
+    Vector3[] navmeshVertices;
+
     private void Start()
     {
         generatorBoundingBox = GetComponent<BoxCollider>();
@@ -28,6 +31,20 @@ public class TacticalPointsGeneratorBox : MonoBehaviour
     public void Generate()
     {
         Debug.Log("Generate clicked");
+        #region Get Navmesh Vertices
+
+
+        NavMeshTriangulation triangulation = NavMesh.CalculateTriangulation();
+        navmeshVertices = new Vector3[triangulation.vertices.Length];
+        for (int i = 0; i < triangulation.vertices.Length; i++)
+        {
+            navmeshVertices[i] = triangulation.vertices[i];
+        }
+
+
+        #endregion
+
+        #region Destroy Previous Children
 
         tacticalPointsToDestroy.Clear();
         foreach (Transform generatedPoint in transform) //theyre all children
@@ -40,6 +57,9 @@ public class TacticalPointsGeneratorBox : MonoBehaviour
             DestroyImmediate(generatedPoint);
         }
 
+        #endregion
+
+        #region Spawn new Points
 
         //get bounding box / collider box edges
         Vector3 lowerLeftPosition = generatorBoundingBox.center - generatorBoundingBox.size/2;
@@ -58,13 +78,48 @@ public class TacticalPointsGeneratorBox : MonoBehaviour
                 NavMeshHit hit;
                 if(NavMesh.SamplePosition(transform.TransformPoint(new Vector3(x, 0, y)), out hit, manager.maxSnapDistanceToNavmesh, NavMesh.AllAreas))
                 {
+                    //1. check if its not too close to the edge of the navmesh -> readjust if needed -> move a little away from navmesh vertex
+                    Vector3 spawnPosition = hit.position;
+
+                    if(NavMesh.FindClosestEdge(spawnPosition, out hit, NavMesh.AllAreas))
+                    {
+                        spawnPosition += (spawnPosition - hit.position).normalized * minDistanceOfGeneratedPointToNavmeshVertex;
+                    }
+
+
+                   /* HashSet<Vector3> verticesUnderMinDistance = new HashSet<Vector3>();
+                    float squaredThreshold = minDistanceOfGeneratedPointToNavmeshVertex * minDistanceOfGeneratedPointToNavmeshVertex;
+                    //float closestSquaredDistance = Mathf.Infinity;
+
+                    for (int i = 0; i < navmeshVertices.Length; i++)
+                    {
+                        float currentSquaredDistance = (navmeshVertices[i] - spawnPosition).sqrMagnitude;
+                        if(currentSquaredDistance < minDistanceOfGeneratedPointToNavmeshVertex * minDistanceOfGeneratedPointToNavmeshVertex)
+                        {
+                            verticesUnderMinDistance.Add(navmeshVertices[i]);
+                        }
+                           // minDistanceOfGeneratedPointToNavmeshVertex
+                    }
+
+                    Vector3 meanVectorAwayFromVetices = Vector3.zero;
+                    foreach (Vector3 vertex in verticesUnderMinDistance)
+                    {
+                        meanVectorAwayFromVetices += (spawnPosition - vertex);
+                    }
+                    meanVectorAwayFromVetices.y = 0;
+
+                    spawnPosition += meanVectorAwayFromVetices.normalized * minDistanceOfGeneratedPointToNavmeshVertex;*/
+
+
+                    //Spawn
                     string prefabPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(manager.openFieldPointPrefab);
                     //Get prefab object from path
                     Object prefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object));
                     //Instantiate the prefab in the scene, as a sibling of current gameObject
-                    GameObject spawnedPoint = PrefabUtility.InstantiatePrefab(prefab, transform.parent) as GameObject;
-                    spawnedPoint.transform.SetParent(transform);
-                    spawnedPoint.transform.position = hit.position;
+                    //GameObject spawnedPoint = PrefabUtility.InstantiatePrefab(prefab, transform.parent) as GameObject;
+                    GameObject spawnedPoint = PrefabUtility.InstantiatePrefab(prefab, transform) as GameObject;
+                    //spawnedPoint.transform.SetParent(transform);
+                    spawnedPoint.transform.position = spawnPosition;
                     spawnedPoint.transform.rotation = Quaternion.identity; //let them all have the same rotation for easier comparing
 
                     TacticalPoint tacticalPoint = spawnedPoint.GetComponent<TacticalPoint>();
@@ -73,8 +128,11 @@ public class TacticalPointsGeneratorBox : MonoBehaviour
 
                 }
             }
-        }  
+        }
+
+        #endregion
     }
+
 
     public void BakeCoverRatings()
     {
