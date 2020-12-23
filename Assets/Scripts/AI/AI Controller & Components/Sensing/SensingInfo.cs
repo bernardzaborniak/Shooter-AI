6de12aa55cbe96d0aefbee1f2863e63f9cc13347
Master-Bernardo <benito4747@live.com>
2020-12-23 13:43:37 +0100

    cleaned up sensing

--- Assets/Scripts/AI/AI Controller & Components/Sensing/SensingInfo.cs
+++ Assets/Scripts/AI/AI Controller & Components/Sensing/SensingInfo.cs
@@ -21,16 +21,15 @@ namespace BenitosAI
     // Custom Object used for saving and transfering sensing information, needs Expanding with adding some kind of memory
     public class SensingInfo
     {
-       
+
         //Enemies
         public SensedEntityInfo nearestEnemyInfo;
         float nearestEnemySquaredDistance;
 
         Queue<SensedEntityInfo> enemyInfoPool = new Queue<SensedEntityInfo>();
-        Dictionary<int,SensedEntityInfo> enemyInfosAddedThisFrame = new Dictionary<int, SensedEntityInfo>();
-        Dictionary<int,SensedEntityInfo> enemyInfosAddedPreviousFrame = new Dictionary<int, SensedEntityInfo>();
+        Dictionary<int, SensedEntityInfo> enemyInfosAddedThisFrame = new Dictionary<int, SensedEntityInfo>();
+        Dictionary<int, SensedEntityInfo> enemyInfosAddedPreviousFrame = new Dictionary<int, SensedEntityInfo>();
         public SensedEntityInfo[] enemyInfos = new SensedEntityInfo[0];
-
 
         //Friendlies
         Queue<SensedEntityInfo> friendlyInfoPool = new Queue<SensedEntityInfo>();
@@ -38,14 +37,13 @@ namespace BenitosAI
         Dictionary<int, SensedEntityInfo> friendlyInfosAddedPreviousFrame = new Dictionary<int, SensedEntityInfo>();
         public SensedEntityInfo[] friendlyInfos = new SensedEntityInfo[0];
 
-
-        //Tactical Points
+        //Tactical Points Cover
         Queue<SensedTacticalPointInfo> tPointCoverInfoPool = new Queue<SensedTacticalPointInfo>();
         Dictionary<int, SensedTacticalPointInfo> tPointCoverInfosAddedThisFrame = new Dictionary<int, SensedTacticalPointInfo>();
         Dictionary<int, SensedTacticalPointInfo> tPointCoverInfosAddedPreviousFrame = new Dictionary<int, SensedTacticalPointInfo>();
         public SensedTacticalPointInfo[] tPointCoverInfos = new SensedTacticalPointInfo[0];
 
-
+        //Tactical Points Open Field
         Queue<SensedTacticalPointInfo> tPointOpenFieldInfoPool = new Queue<SensedTacticalPointInfo>();
         Dictionary<int, SensedTacticalPointInfo> tPointOpenFieldInfosAddedThisFrame = new Dictionary<int, SensedTacticalPointInfo>();
         Dictionary<int, SensedTacticalPointInfo> tPointOpenFieldInfosAddedPreviousFrame = new Dictionary<int, SensedTacticalPointInfo>();
@@ -83,40 +81,6 @@ namespace BenitosAI
         public void OnSensedEnemyEntity(EntityVisibilityInfo enemyEntityVisInfo, float squaredDistance)
         {
             OnSensedEntity(enemyEntityVisInfo, squaredDistance, ref enemyInfoPool, ref enemyInfosAddedThisFrame, ref enemyInfosAddedPreviousFrame);
-            /*int key = enemyEntityVisInfo.entityAssignedTo.GetHashCode();
-
-            SensedEntityInfo sensedEntityInfo;
-
-            // Update older information with new one
-            if (enemyInfosAddedPreviousFrame.ContainsKey(key))
-            {
-                sensedEntityInfo = enemyInfosAddedPreviousFrame[key];
-                enemyInfosAddedPreviousFrame.Remove(key);
-                infosAddedThisFrame.Add(key, sensedEntityInfo);
-            }
-            // Else Get An info Object from the pool
-            else if(enemiesInfosPool.Count > 0)
-            {
-                sensedEntityInfo = enemiesInfosPool.Dequeue();
-                infosAddedThisFrame.Add(key, sensedEntityInfo);
-            }
-            //If pool is empty, overwrite and older info
-            else
-            {
-                //just take the first one of the old
-                int keyToOverride = 0;
-                foreach(int previousKey in enemyInfosAddedPreviousFrame.Keys)
-                {
-                    keyToOverride = previousKey;
-                    break;
-                }
-
-                sensedEntityInfo = enemyInfosAddedPreviousFrame[keyToOverride];
-                enemyInfosAddedPreviousFrame.Remove(keyToOverride);
-                infosAddedThisFrame.Add(keyToOverride, sensedEntityInfo);
-            }
-
-            sensedEntityInfo.SetUpInfo(enemyEntityVisInfo, squaredDistance);*/
         }
 
         public void OnSensedFriendlyEntity(EntityVisibilityInfo friendlyEntityVisInfo, float squaredDistance)
@@ -128,38 +92,44 @@ namespace BenitosAI
         {
             int key = entityVisInfo.entityAssignedTo.GetHashCode();
 
-           SensedEntityInfo sensedEntityInfo;
+            SensedEntityInfo sensedEntityInfo;
 
-           // Update older information with new one
-           if (infosAddedPreviousFrames.ContainsKey(key))
-           {
-               sensedEntityInfo = infosAddedPreviousFrames[key];
-               infosAddedPreviousFrames.Remove(key);
-               infosAddedThisFrame.Add(key, sensedEntityInfo);
-           }
-           // Else Get An info Object from the pool
-           else if(infosPool.Count > 0)
-           {
-               sensedEntityInfo = infosPool.Dequeue();
-               infosAddedThisFrame.Add(key, sensedEntityInfo);
-           }
-           //If pool is empty, overwrite and older info
-           else
-           {
-               //just take the first one of the old
-               int keyToOverride = 0;
-               foreach(int previousKey in infosAddedPreviousFrames.Keys)
-               {
-                   keyToOverride = previousKey;
-                   break;
-               }
+            // Update older information with new one
+            if (infosAddedPreviousFrames.ContainsKey(key))
+            {
+                sensedEntityInfo = infosAddedPreviousFrames[key];
+                infosAddedPreviousFrames.Remove(key);
+                infosAddedThisFrame.Add(key, sensedEntityInfo);
 
-               sensedEntityInfo = infosAddedPreviousFrames[keyToOverride];
+                sensedEntityInfo.SetUpInfo(entityVisInfo, squaredDistance);
+            }
+            // Else Get An info Object from the pool
+            else if (infosPool.Count > 0)
+            {
+                sensedEntityInfo = infosPool.Dequeue();
+                infosAddedThisFrame.Add(key, sensedEntityInfo);
+
+                sensedEntityInfo.SetUpInfo(entityVisInfo, squaredDistance);
+
+            }
+            //If pool is empty, overwrite and older info
+            else if (infosAddedPreviousFrames.Count > 0)
+            {
+                //just take the first one of the old
+                int keyToOverride = 0;
+                foreach (int previousKey in infosAddedPreviousFrames.Keys)
+                {
+                    keyToOverride = previousKey;
+                    break;
+                }
+
+                sensedEntityInfo = infosAddedPreviousFrames[keyToOverride];
                 infosAddedPreviousFrames.Remove(keyToOverride);
-               infosAddedThisFrame.Add(keyToOverride, sensedEntityInfo);
-           }
+                infosAddedThisFrame.Add(keyToOverride, sensedEntityInfo);
 
-           sensedEntityInfo.SetUpInfo(entityVisInfo, squaredDistance);
+                sensedEntityInfo.SetUpInfo(entityVisInfo, squaredDistance);
+            }  //else: it can happen that there are more infos added this frame than the pool size, thoose extra infos are just ignored, maybe also adjust the collider pool SIze inside sensing
+
         }
 
         #endregion
@@ -181,12 +151,9 @@ namespace BenitosAI
             int key = tPointVisInfo.tacticalPointAssignedTo.GetHashCode();
 
             SensedTacticalPointInfo sensedTPointInfo;
-            Debug.Log("one sensed TPoint");
             // Update older information with new one
             if (infosAddedPreviousFrames.ContainsKey(key))
             {
-                Debug.Log("one sensed 1");
-
                 sensedTPointInfo = infosAddedPreviousFrames[key];
                 infosAddedPreviousFrames.Remove(key);
                 infosAddedThisFrame.Add(key, sensedTPointInfo);
@@ -197,8 +164,6 @@ namespace BenitosAI
             // Else Get An info Object from the pool
             else if (infosPool.Count > 0)
             {
-                Debug.Log("one sensed 2");
-
                 sensedTPointInfo = infosPool.Dequeue();
                 infosAddedThisFrame.Add(key, sensedTPointInfo);
 
@@ -206,28 +171,24 @@ namespace BenitosAI
 
             }
             //If pool is empty, overwrite and older info
-            else
+            else if (infosAddedPreviousFrames.Count > 0)
             {
-                Debug.Log("one sensed 3");
-                if (infosAddedPreviousFrames.Count > 0)
+                //just take the first one of the old
+                int keyToOverride = 0;
+                foreach (int previousKey in infosAddedPreviousFrames.Keys)
                 {
-                    //just take the first one of the old
-                    int keyToOverride = 0;
-                    foreach (int previousKey in infosAddedPreviousFrames.Keys)
-                    {
-                        keyToOverride = previousKey;
-                        break;
-                    }
-
-                    sensedTPointInfo = infosAddedPreviousFrames[keyToOverride];
-                    infosAddedPreviousFrames.Remove(keyToOverride);
-                    infosAddedThisFrame.Add(keyToOverride, sensedTPointInfo);
-
-                    sensedTPointInfo.SetUpInfo(tPointVisInfo, squaredDistance);
+                    keyToOverride = previousKey;
+                    break;
                 }
-                //else: it can happen that there are more infos added this frame than the pool size, thoose extra infos are just ignored, maybe also adjust the collider pool SIze inside sensing
 
-            }
+                sensedTPointInfo = infosAddedPreviousFrames[keyToOverride];
+                infosAddedPreviousFrames.Remove(keyToOverride);
+                infosAddedThisFrame.Add(keyToOverride, sensedTPointInfo);
+
+                sensedTPointInfo.SetUpInfo(tPointVisInfo, squaredDistance);
+
+            }  //else: it can happen that there are more infos added this frame than the pool size, thoose extra infos are just ignored, maybe also adjust the collider pool SIze inside sensing
+
 
         }
 
@@ -237,51 +198,13 @@ namespace BenitosAI
 
         public void UpdateSensingInfoAfterAddingNewInfo()
         {
-            Debug.Log("-----Update After Adding Info ----");
             UpdateEntities(ref enemyInfos, ref enemyInfoPool, ref enemyInfosAddedThisFrame, ref enemyInfosAddedPreviousFrame);
             UpdateEntities(ref friendlyInfos, ref friendlyInfoPool, ref friendlyInfosAddedThisFrame, ref friendlyInfosAddedPreviousFrame);
 
             UpdateTPoints(ref tPointCoverInfos, ref tPointCoverInfoPool, ref tPointCoverInfosAddedThisFrame, ref tPointCoverInfosAddedPreviousFrame);
             UpdateTPoints(ref tPointOpenFieldInfos, ref tPointOpenFieldInfoPool, ref tPointOpenFieldInfosAddedThisFrame, ref tPointOpenFieldInfosAddedPreviousFrame);
-            /*HashSet<int> deadOnesToRemoveIDs = new HashSet<int>();
-
-            //cheack for dead ones
-            foreach (SensedEntityInfo item in enemyInfosAddedPreviousFrame.Values)
-            {
-                if(!item.IsAlive())
-                {
-                    deadOnesToRemoveIDs.Add(item.GetHashCode());
-                }
-            }
-            foreach (int key in deadOnesToRemoveIDs)
-            {
-                enemyInfosPool.Enqueue(enemyInfosAddedPreviousFrame[key]);
-                enemyInfosAddedPreviousFrame.Remove(key);
-            }
-
-
-            //Fill the array,
-            enemyInfo = new SensedEntityInfo[enemyInfosAddedThisFrame.Count + enemyInfosAddedPreviousFrame.Count];
-
-            enemyInfosAddedThisFrame.Values.CopyTo(enemyInfo, 0);
-            enemyInfosAddedPreviousFrame.Values.CopyTo(enemyInfo, enemyInfosAddedThisFrame.Count);
-
-          
-            // Clear up current, fill the previous
-            foreach (SensedEntityInfo item in enemyInfosAddedThisFrame.Values)
-            {
-                Debug.Log("item: " + item);
-                Debug.Log("item.entity.GetHashCode(): " + item.GetHashCode()); //TODO NULL here
-                enemyInfosAddedPreviousFrame.Add(item.GetHashCode(), item);
-            }
-            
-            enemyInfosAddedThisFrame.Clear();
-
-            //Sort the array by time when updated (optionally) 
-
 
             //set the nearest just like that for now
-            */
             if (enemyInfos.Length > 0)
             {
                 nearestEnemyInfo = enemyInfos[0];
@@ -369,7 +292,7 @@ namespace BenitosAI
             //Sort the array by time when updated (optionally) 
         }
 
-#endregion
+        #endregion
     }
 
 }
