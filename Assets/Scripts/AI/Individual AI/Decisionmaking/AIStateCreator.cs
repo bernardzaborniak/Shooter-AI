5c0ca7356ff96ce8e78423189d8d351f55213f5f
Master-Bernardo <benito4747@live.com>
2021-03-13 17:15:43 +0100

    changed order of consideration for the shoot decision

--- Assets/Scripts/AI/Individual AI/Decisionmaking/AIStateCreator.cs
+++ Assets/Scripts/AI/Individual AI/Decisionmaking/AIStateCreator.cs
@@ -6,16 +6,16 @@ namespace BenitosAI
 {
     public abstract class AIStateCreator : ScriptableObject
     {
-        //public AIStateCreatorInputParams inputParams;
-
         [HideInInspector]
         public AIStateCreatorInputParams.InputParamsType[] inputParamsType;
 
+        // Creates and returns an AIState object.
         public abstract AIState CreateState(AIController aiController, DecisionContext context, AIStateCreatorInputParams inputParams);
     }
 
     public abstract class AIState
     {
+        // Deriving classes will all implement their own Constructor with needed info as parameters.
         //public abstract void SetUpState(AIController aiController, DecisionContext context);
 
         public abstract void OnStateEnter();
@@ -29,7 +29,5 @@ namespace BenitosAI
         public abstract void UpdateState();
 
         public abstract bool ShouldStateBeAborted();
-
-
     }
 }
